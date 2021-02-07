b9b8283908d29c5a5e731698374871362cc74b65
mfj123456 <2915350015@qq.com>
2021-02-07 18:30:35 +0800

    构造函数的get和set

--- test01/src/test01/aaa.java
+++ test01/src/test01/aaa.java
@@ -4,5 +4,41 @@ public class aaa {
 
 	public String name;
 	public Integer age;
-                public Integer a
+	public Integer a;
+
+	public aaa(String name, Integer age, Integer a) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.a = a;
+	}
+
+	public aaa() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getA() {
+		return a;
+	}
+
+	public void setA(Integer a) {
+		this.a = a;
+	}
+
 }
